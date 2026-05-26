4a12a33cc6138d22012366b88d4bb62957a07657
25-0009C <25-0009C@CCL1-23>
2026-05-26 09:47:13 +0800

    Marco

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -10,7 +10,48 @@ namespace ConsoleApp40
     {
         static void Main(string[] args)
         {
+            string day = "";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write('C');Console.ResetColor();
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.Write('O');Console.ResetColor();
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.Write('L');Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write('O');Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write('R');Console.ResetColor();
+            Console.Write("\t");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write('L');Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write('A'); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write('B');Console.ResetColor();
+            Console.WriteLine("\n");
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("█████ ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("█████ ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("█████ ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("█████ ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("█████ ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.Write(" [1]   " +"[2]  " + " [3]   " + "[4]   " + "[5]");
+            color(day);
+        }
+        static void color (string day)
+        {
+            
         }
     }
 }
